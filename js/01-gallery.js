d7c5ef751e97dfa9e2381973223ce93437dd528b
Bob-UA <120073426+Bob-UA@users.noreply.github.com>
2023-04-19 21:45:55 -0700

    added_elements_to_gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,22 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector(".gallery");
 
-console.log(galleryItems);
+const markupCards = createGalleryCardsMarckup(galleryItems);
+console.log(markupCards);
+gallery.insertAdjacentHTML("afterbegin", markupCards)
+
+function createGalleryCardsMarckup(elements) {
+    return elements.map(
+        ({ preview, original, description }) => {
+            return `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`;}).join("");
+}
