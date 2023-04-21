508cf34d31e87f23c0a279a8f4e1a98df47bba8f
Bob-UA <120073426+Bob-UA@users.noreply.github.com>
2023-04-20 21:19:50 -0700

    task-1_completed

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,9 +1,9 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+
 const gallery = document.querySelector(".gallery");
 
 const markupCards = createGalleryCardsMarckup(galleryItems);
-console.log(markupCards);
 gallery.insertAdjacentHTML("afterbegin", markupCards)
 
 function createGalleryCardsMarckup(elements) {
@@ -20,3 +20,33 @@ function createGalleryCardsMarckup(elements) {
   </a>
 </li>`;}).join("");
 }
+
+const onElementClick= ((event) => {
+    event.preventDefault();
+    const originalImg = event.target.attributes[2].nodeValue;
+    const isImageEl = event.target.classList.contains("gallery__image");
+    
+    if (!isImageEl) {
+        return;
+    }
+    const instance = basicLightbox.create(`
+    <img src="${originalImg}" width="800" height="600">
+`);
+    
+    instance.show();
+
+    const handleClick = (event) => {
+      console.log("asdasd");
+      if (event.code === "Escape") {
+          instance.close();
+          document.removeEventListener("keydown", handleClick);
+      }
+    };
+    
+    document.addEventListener("keydown", handleClick);
+})
+
+
+
+gallery.addEventListener("click", onElementClick);
+
