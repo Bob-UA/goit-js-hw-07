cd99c6e9d86b72a0cd5a2c19f858c2739b01e9c5
Bob-UA <120073426+Bob-UA@users.noreply.github.com>
2023-04-21 20:47:30 -0700

    2-atask_gallery_is_working

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,39 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+const markupCards = createGalleryCardsMarckup(galleryItems);
+gallery.insertAdjacentHTML("afterbegin", markupCards);
+
+function createGalleryCardsMarckup(elements) {
+  return elements
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+          <a class="gallery__link" href="${original}">
+            <img
+              class="gallery__image"
+              src="${preview}"
+              alt="${description}"
+            />
+          </a>
+        </li>`;
+    })
+    .join("");
+}
+
+const onElementClick = (e) => {
+    e.preventDefault();
+    let gallery = new SimpleLightbox(".gallery a");
+    gallery.on("show.simplelightbox", function () {
+    });
+
+    gallery.on("error.simplelightbox", function (e) {
+      console.log(e);
+    });
+};
+
+gallery.addEventListener("click", onElementClick);
+
+
+
